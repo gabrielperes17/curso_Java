9dfef235ab324fe7254de9024cb5d3fca7e915a3
gabriel Peres <105747318+gabrielperes17@users.noreply.github.com>
2024-12-10 21:29:41 -0300

    Add files via upload

---
+++ ProjectMain.java
@@ -0,0 +1,11 @@
+package com.mycompany.projectmain;
+
+import java.util.Date;
+public class ProjectMain {
+
+    public static void main(String[] args) {
+        Date relogio= new Date();
+        System.out.print("Hora Marcada: ");
+        System.out.println(relogio.toString());
+    }
+}
