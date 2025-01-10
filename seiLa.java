a53eea71b56848b8453099bca0cc2ac81e5ae7ea
gabriel Peres <105747318+gabrielperes17@users.noreply.github.com>
2025-01-10 19:48:26 -0300

    Add files via upload

---
+++ seiLa.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projectmain;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Cliente
+ */
+public class seiLa {
+    public static void main(String args){
+        Scanner numsei= new Scanner(System.in);
+        System.out.println("Quantas pernas?");
+        int pernas= numsei.nextInt();
+        String tipo;
+        System.out.println("Isto é um(a)");
+        switch (pernas){
+            case 1:
+                tipo="saci";
+                break;
+            case 2:
+                tipo="Bipede";
+                break;
+            case 3:
+                tipo="Cavalho de 3 Pernas! ksksksk";
+                break;
+            default:
+                case 4:
+                tipo="ET";
+                
+                
+        
+        
+    }
+   }
+}
