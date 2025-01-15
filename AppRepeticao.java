3270e590d0de362b979dae9ea8ab7a5f9e5174ed
gabriel Peres <105747318+gabrielperes17@users.noreply.github.com>
2025-01-15 19:44:47 -0300

    Add files via upload

---
+++ AppRepeticao.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projectmain;
+
+/**
+ *
+ * @author Cliente
+ */
+public class AppRepeticao {
+    public static void main(String args[]){
+        int cc=0;
+        while (cc<100){
+            System.out.println(cc);
+            cc++;
+        }
+    }
+}
