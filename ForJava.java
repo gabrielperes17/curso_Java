d67fdb1c43b1fb07a91b18b8eb7679f6e79147be
gabriel Peres <105747318+gabrielperes17@users.noreply.github.com>
2025-01-27 19:26:26 -0300

    Add files via upload

---
+++ ForJava.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projectmain;
+
+/**
+ *
+ * @author Cliente
+ */
+public class ForJava {
+    public static void main(String[] args){
+        for(int c=0;c<=100;c=c+10){
+            System.out.println(c);
+        }
+    }
+}
