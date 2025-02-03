0a3e96b3cb88266c73ccf11d9085002447e4e0b6
gabriel Peres <105747318+gabrielperes17@users.noreply.github.com>
2025-02-03 20:51:56 -0300

    Add files via upload

---
+++ class18.java
@@ -0,0 +1,19 @@
+package com.mycompany.projectmain;
+
+import java.util.ArrayList;
+//vetores ou arrays
+public class class18 {
+    public static void main(String[] args) {
+        int[] vetoresnuns = {0, 1, 2, 3};
+        System.out.println(vetoresnuns[1]);
+    }
+}
+// ArrayList
+class Seila{
+    public static void main(String[] args){
+        ArrayList<Integer> idades=new ArrayList<>();
+        idades.add(12);
+        idades.add(17);
+        idades.remove(0);
+    }
+}
